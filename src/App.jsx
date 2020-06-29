05fe656f062aff9658f6777ebee28e6314db9a70
Abtahee Ali <abtaheeali@gmail.com>
2020-06-29 15:02:22 -0400

    Added routes to app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import CategoryList from './components/CategoryList'
+import { PhotoList } from './components/PhotoList'
+import { PhotoDetails } from './components/PhotoDetails'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <body className="bg-light">
+          <Header />
+          <main>
+            <Route exact path="/" component={CategoryList} />
+            <Route
+              exact
+              path="/:theNameOfTheCategoryWeWantToLookAt"
+              component={PhotoList}
+            />
+            <Route
+              path="/:theNameOfTheCategoryWeWantToLookAt/:arrayIndexOfThePhoto"
+              component={PhotoDetails}
+            />
+          </main>
+        </body>
+      </Router>
+    )
+  }
+}
