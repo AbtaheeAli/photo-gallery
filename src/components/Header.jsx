4c5f897ddbd1ae8696379c8f3b04bb0cf5202829
Abtahee Ali <abtaheeali@gmail.com>
2020-06-29 15:04:12 -0400

    Added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <Link to="/">
+            <h1 className="display-4">Things I Enjoy</h1>{' '}
+          </Link>
+          <p class="lead">These are the things that I enjoy</p>
+        </div>
+      </header>
+    )
+  }
+}
