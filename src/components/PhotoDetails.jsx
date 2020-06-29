256f83228783c044b9862e2d648c88085a43b825
Abtahee Ali <abtaheeali@gmail.com>
2020-06-29 15:05:44 -0400

    Added photolist and photodetails

---
+++ src/components/PhotoDetails.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Gallery from '../gallery.json'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+
+export class PhotoDetails extends Component {
+  render() {
+    const theNameOfTheCategoryWeWantToLookAt = this.props.match.params
+      .theNameOfTheCategoryWeWantToLookAt
+
+    const arrayIndexOfThePhoto = this.props.match.params.arrayIndexOfThePhoto
+
+    const category = Gallery[theNameOfTheCategoryWeWantToLookAt]
+
+    const categoryPhotos = category.photos
+
+    const photo = categoryPhotos[arrayIndexOfThePhoto]
+
+    return (
+      <div>
+        <section>
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-light">
+              <li class="breadcrumb-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="breadcrumb-item">
+                <Link
+                  class="breadcrumb-item"
+                  to={`/${theNameOfTheCategoryWeWantToLookAt}`}
+                >
+                  {category.title}
+                </Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                <Link
+                  class="breadcrumb-item active"
+                  aria-current="page"
+                  to={`/${theNameOfTheCategoryWeWantToLookAt}/${arrayIndexOfThePhoto}`}
+                >
+                  {photo.title}
+                </Link>
+              </li>
+            </ol>
+          </nav>
+        </section>
+        <div className="container">
+          <h3 className="title">{photo.title}</h3>
+          <figure className="image">
+            <img src={photo.imageURL} alt={photo.title} />
+          </figure>
+          <p className="is-small">
+            <a href={photo.sourceURL}>Source</a>
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/PhotoList.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Gallery from '../gallery.json'
+
+class PhotoListPhoto extends Component {
+  render() {
+    return (
+      <div key={this.props.index} className="column is-one-third">
+        <div class="card">
+          <div class="card-body">
+            <Link
+              to={`/${this.props.theNameOfTheCategoryWeWantToLookAt}/${this.props.index}`}
+            >
+              <img
+                class="card-img-top"
+                src={this.props.photo.imageURL}
+                alt={this.props.photo.title}
+              />
+            </Link>
+            <figcaption>
+              <Link
+                to={`/${this.props.theNameOfTheCategoryWeWantToLookAt}/${this.props.index}`}
+              >
+                {this.props.photo.title}
+              </Link>
+            </figcaption>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export class PhotoList extends Component {
+  render() {
+    const theNameOfTheCategoryWeWantToLookAt = this.props.match.params
+      .theNameOfTheCategoryWeWantToLookAt
+
+    const category = Gallery[theNameOfTheCategoryWeWantToLookAt]
+    const categoryPhotos = category.photos
+
+    return (
+      <div>
+        <section>
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-light">
+              <li class="breadcrumb-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                <Link
+                  class="breadcrumb-item active"
+                  aria-current="page"
+                  to={`/${theNameOfTheCategoryWeWantToLookAt}`}
+                >
+                  {category.title}
+                </Link>
+              </li>
+            </ol>
+          </nav>
+        </section>
+
+        <h3 className="title pl-4">{category.title}</h3>
+        <h4 className="subtitle pl-5">{category.description}</h4>
+        <section className="cards d-flex flex-row justify-content-center flex-wrap">
+          {categoryPhotos.map((photo, index) => (
+            <PhotoListPhoto
+              key={index}
+              index={index}
+              photo={photo}
+              theNameOfTheCategoryWeWantToLookAt={
+                theNameOfTheCategoryWeWantToLookAt
+              }
+            />
+          ))}
+        </section>
+      </div>
+    )
+  }
+}
