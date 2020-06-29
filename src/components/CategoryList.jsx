ab45422b9554fb68c8441442ee99f6d9b9287dd7
Abtahee Ali <abtaheeali@gmail.com>
2020-06-29 15:03:46 -0400

    Added route for category list

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import Gallery from '../gallery.json'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+
+class Category extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <h2>
+            <Link to={`/${this.props.categoryName}`}>{this.props.title}</Link>
+          </h2>
+          <p className="text">{this.props.description}</p>
+        </div>
+        <img
+          class="card-img-top"
+          src={this.props.imageURL}
+          alt={this.props.title}
+        />
+      </div>
+    )
+  }
+}
+
+export default class CategoryList extends Component {
+  render() {
+    const categoryNames = Object.keys(Gallery)
+
+    return (
+      <div>
+        <section>
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb bg-light">
+              <li class="breadcrumb-item">
+                <Link class="breadcrumb-item" to="/">
+                  Home
+                </Link>
+              </li>
+            </ol>
+          </nav>
+        </section>
+        <section className="cards d-flex flex-row justify-content-center flex-wrap">
+          {categoryNames.map((categoryName, index) => {
+            const categoryDetails = Gallery[categoryName]
+            const firstPhoto = categoryDetails.photos[0]
+
+            return (
+              <Category
+                key={index}
+                categoryName={categoryName}
+                title={categoryDetails.title}
+                description={categoryDetails.description}
+                imageURL={firstPhoto.imageURL}
+              />
+            )
+          })}
+        </section>
+      </div>
+    )
+  }
+}
